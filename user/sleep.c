b344ab6ce538d346cbb209a205587388bc5c8177
chensx <csx0129@gmail.com>
2024-04-25 21:44:38 +0800

    exercise 1 : sleep

--- user/sleep.c
+++ user/sleep.c
@@ -1,14 +1,16 @@
-#include "user/user.h"
 #include "kernel/types.h"
 #include "kernel/stat.h"
+#include "user/user.h"
 
-int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    fprintf(2, "usage: sleep [ticks num]\n");
+int main(int argc, char *argv[]) 
+{
+  if (argc != 2) 
+  {
+    printf ("wrong argc");
     exit(1);
   }
-  // atoi sys call guarantees return an integer
+
   int ticks = atoi(argv[1]);
-  int ret = sleep(ticks);
-  exit(ret);
+  sleep(ticks);
+  exit(0);
 }
